e62c2d94411e89c17268f8fbb8da4b5d2ea86ece
Rahul Ghadage <54259667+rahul-ghadge@users.noreply.github.com>
2020-01-03 14:25:01 +0530

    Create Configuration.java

---
+++ AbstractFactoryPattern/src/main/java/com/arya/demo/abstractfactory/Configuration.java
@@ -0,0 +1,16 @@
+package com.arya.demo.abstractfactory;
+
+public abstract class Configuration {
+	
+	public abstract String getRAM();
+
+	public abstract String getHDD();
+
+	public abstract String getCPU();
+
+	
+	@Override
+	public String toString() {
+		return "RAM : " + this.getRAM() + ", HDD : " + this.getHDD() + ", CPU : " + this.getCPU();
+	}
+}
