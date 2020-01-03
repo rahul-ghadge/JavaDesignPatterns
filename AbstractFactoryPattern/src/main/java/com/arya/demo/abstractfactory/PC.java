890c42f6b536b960f0811afc1eaf6c60b97cc0e7
Rahul Ghadage <54259667+rahul-ghadge@users.noreply.github.com>
2020-01-03 14:25:40 +0530

    Create PC.java

---
+++ AbstractFactoryPattern/src/main/java/com/arya/demo/abstractfactory/PC.java
@@ -0,0 +1,34 @@
+package com.arya.demo.abstractfactory;
+
+public class PC extends Configuration {
+	
+	private String ram;
+	private String hdd;
+	private String cpu;
+	
+	public PC(String ram, String hdd, String cpu) {
+		this.ram = ram;
+		this.hdd = hdd;
+		this.cpu = cpu;
+	}
+
+	@Override
+	public String getRAM() {
+		return this.ram + " GB for PC";
+	}
+
+	@Override
+	public String getHDD() {
+		return this.hdd+ " GB for PC";
+	}
+
+	@Override
+	public String getCPU() {
+		return this.cpu+ " GHz Processor for PC";
+	}
+
+	@Override
+	public String toString() {
+		return "PC [ram=" + ram + ", hdd=" + hdd + ", cpu=" + cpu + "]";
+	}
+}
