3152be3d4cdcc2ce671064b2ed786d3af13e10f4
Rahul Ghadage <54259667+rahul-ghadge@users.noreply.github.com>
2020-01-03 14:27:02 +0530

    Create ServerConfigurationFactory.java

---
+++ AbstractFactoryPattern/src/main/java/com/arya/demo/abstractfactory/ServerConfigurationFactory.java
@@ -0,0 +1,20 @@
+package com.arya.demo.abstractfactory;
+
+public class ServerConfigurationFactory implements ConfigurationAbstractFactory {
+
+	private String ram;
+	private String hdd;
+	private String cpu;
+
+	public ServerConfigurationFactory(String ram, String hdd, String cpu) {
+		this.ram = ram;
+		this.hdd = hdd;
+		this.cpu = cpu;
+	}
+
+	@Override
+	public Configuration getConfiguration() {
+		return new Server(ram, hdd, cpu);
+	}
+
+}
